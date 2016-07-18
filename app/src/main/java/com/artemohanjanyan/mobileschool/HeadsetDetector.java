486d25143b1d6d4f77bd50c74c9cbc791ca53864
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2016-07-19 01:04:06 +0300

    task 4 done

--- app/src/main/java/com/artemohanjanyan/mobileschool/HeadsetDetector.java
+++ app/src/main/java/com/artemohanjanyan/mobileschool/HeadsetDetector.java
@@ -7,48 +7,58 @@ import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.preference.PreferenceManager;
+import android.support.annotation.StringRes;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
-import android.util.Log;
 
 public class HeadsetDetector extends BroadcastReceiver {
-    private static final int NOTIFICATION_ID = 1;
+    private static final int MUSIC_NOTIFICATION_ID = 1;
+    private static final int RADIO_NOTIFICATION_ID = 2;
     private static final String MUSIC_PACKAGE = "ru.yandex.music";
+    private static final String RADIO_PACKAGE = "ru.yandex.radio";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         int state = intent.getIntExtra("state", -1);
 
-        if (PreferenceManager.getDefaultSharedPreferences(context)
-                .getBoolean("switch_music", true)) {
-            Log.d(HeadsetDetector.class.getSimpleName(), "processing music notification...");
+        if (state == 1) {
+            showNotification(context, MUSIC_PACKAGE, R.string.preference_music,
+                    R.string.open_music, MUSIC_NOTIFICATION_ID);
+            showNotification(context, RADIO_PACKAGE, R.string.preference_radio,
+                    R.string.open_radio, RADIO_NOTIFICATION_ID);
+        } else if (state == 0) {
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            notificationManager.cancel(MUSIC_NOTIFICATION_ID);
+            notificationManager.cancel(RADIO_NOTIFICATION_ID);
+        }
+    }
 
-            if (state == 1) {
-                Intent launchIntent =
-                        context.getPackageManager().getLaunchIntentForPackage(MUSIC_PACKAGE);
-                if (launchIntent != null) {
-                    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-                            launchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-                    NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                            .setSmallIcon(R.drawable.notification)
-                            .setContentTitle(context.getString(R.string.open_music))
-                            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                            .setContentIntent(pendingIntent);
-                    try {
-                        Drawable icon =
-                                context.getPackageManager().getApplicationIcon(MUSIC_PACKAGE);
-                        builder.setLargeIcon(((BitmapDrawable) icon).getBitmap());
-                    } catch (Exception ignored) {
-                    }
-                    NotificationManagerCompat notificationManager =
-                            NotificationManagerCompat.from(context);
-                    notificationManager.notify(NOTIFICATION_ID, builder.build());
-                }
-            } else if (state == 0) {
-                NotificationManagerCompat notificationManager =
-                        NotificationManagerCompat.from(context);
-                notificationManager.cancel(NOTIFICATION_ID);
+    private void showNotification(Context context,
+                                  String appPackage, @StringRes int preferenceRes,
+                                  @StringRes int notificationText, int notificationId) {
+        if (!PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(context.getString(preferenceRes), true)) {
+            return;
+        }
+
+        Intent launchIntent =
+                context.getPackageManager().getLaunchIntentForPackage(appPackage);
+        if (launchIntent != null) {
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                    launchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.drawable.notification)
+                    .setContentTitle(context.getString(R.string.headphones_plugged_in))
+                    .setContentText(context.getString(notificationText))
+                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                    .setContentIntent(pendingIntent);
+            try {
+                Drawable icon = context.getPackageManager().getApplicationIcon(appPackage);
+                builder.setLargeIcon(((BitmapDrawable) icon).getBitmap());
+            } catch (Exception ignored) {
             }
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            notificationManager.notify(notificationId, builder.build());
         }
     }
 }
